7ba362354f4c0eee589f3f5da88591638aadd969
RobotsBuildingEducation <robotsbuildingeducation@gmail.com>
2025-12-17 14:24:29 -0600

    store

--- nosabos/src/main.jsx
+++ nosabos/src/main.jsx
@@ -3,11 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { theme } from "./theme";
 import LandingPage from "./components/LandingPage.jsx";
 import WalletExperiment from "./components/WalletExperiment.jsx";
@@ -49,7 +45,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/" element={<AppContainer />} />
         <Route path="/onboarding/*" element={<AppContainer />} />
         <Route path="/subscribe" element={<AppContainer />} />
-        <Route path="/experiments" element={<WalletExperiment />} />
+        {/* <Route path="/experiments" element={<WalletExperiment />} /> */}
       </Routes>
     </Router>
   </ChakraProvider>
