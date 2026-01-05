d9a97a87b7c08d3194db0200a3cfbc2432b06d34
RobotsBuildingEducation <robotsbuildingeducation@gmail.com>
2026-01-04 19:40:14 -0600

    store

--- nosabos/dev-dist/sw.js
+++ nosabos/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kmfsp934a3o"
+    "revision": "0.cfo6jtnqomg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
